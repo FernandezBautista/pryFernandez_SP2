7ef70e5c1fa4cf38fac56db1d4cbb49d0cc83c3e
Bautista <fernandezbautistauala@gmail.com>
2025-09-18 19:04:39 -0300

    Agregar archivos de proyecto.

---
+++ SP02/frmBoletos.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SP02
+{
+    partial class frmBoletos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.Distancia = new System.Windows.Forms.Label();
+            this.nudDias = new System.Windows.Forms.NumericUpDown();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistancia.Location = new System.Drawing.Point(23, 131);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(132, 24);
+            this.lblDistancia.TabIndex = 0;
+            this.lblDistancia.Text = "Distancia (km):";
+            this.lblDistancia.Click += new System.EventHandler(this.lblDistancia_Click);
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(258, 131);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(120, 20);
+            this.txtDistancia.TabIndex = 1;
+            // 
+            // Distancia
+            // 
+            this.Distancia.AutoSize = true;
+            this.Distancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Distancia.Location = new System.Drawing.Point(23, 176);
+            this.Distancia.Name = "Distancia";
+            this.Distancia.Size = new System.Drawing.Size(207, 24);
+            this.Distancia.TabIndex = 2;
+            this.Distancia.Text = "Días de estancia (1–30):";
+            this.Distancia.Click += new System.EventHandler(this.Distancia_Click);
+            // 
+            // nudDias
+            // 
+            this.nudDias.Location = new System.Drawing.Point(258, 181);
+            this.nudDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudDias.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDias.Name = "nudDias";
+            this.nudDias.Size = new System.Drawing.Size(120, 20);
+            this.nudDias.TabIndex = 3;
+            this.nudDias.TabStop = false;
+            this.nudDias.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(80, 242);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(258, 242);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // frmBoletos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 379);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.nudDias);
+            this.Controls.Add(this.Distancia);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.lblDistancia);
+            this.Name = "frmBoletos";
+            this.Text = "Cálculo de boletos de tren";
+            this.Load += new System.EventHandler(this.frmBoletos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.Label Distancia;
+        private System.Windows.Forms.NumericUpDown nudDias;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ SP02/frmBoletos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP02
+{
+    public partial class frmBoletos : Form
+    {
+        public frmBoletos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBoletos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int distancia = int.Parse(txtDistancia.Text);
+            int dias = (int)nudDias.Value;
+            const int PRECIO_POR_KM = 5;
+            int distanciaTotal = distancia * 2;
+            int precioBase = distanciaTotal * PRECIO_POR_KM;
+            bool hayDescuento = (dias >= 7) && (distancia >= 100);
+            int precioFinal = hayDescuento ? (precioBase / 2) : precioBase;
+           
+            MessageBox.Show(
+                "Distancia de ida: " + distancia + " km\n" +
+                "Distancia total: " + distanciaTotal + " km\n" +
+                "Días de estancia: " + dias + "\n" +
+                (hayDescuento ? "Descuento aplicado: 50%\n" : "") +
+                "Precio final: $ " + precioFinal,
+                "Cálculo de boleto",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                            );
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtDistancia.Clear();
+            nudDias.Value = 1;
+        }
+
+        private void lblDistancia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Distancia_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
