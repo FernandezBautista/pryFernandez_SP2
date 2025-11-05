4a3f88b084c8806dd2561add92ee1e9d4f637561
Alumno <Alumno@PC45>
2025-11-05 13:19:59 -0300

    Cambios finales

--- SP02/frmBoletos.Designer.cs
+++ SP02/frmBoletos.Designer.cs
@@ -34,14 +34,21 @@
             this.nudDias = new System.Windows.Forms.NumericUpDown();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
+            this.tabBoletos = new System.Windows.Forms.TabControl();
+            this.tabRegistro = new System.Windows.Forms.TabPage();
+            this.tabConsultas = new System.Windows.Forms.TabPage();
+            this.lstBoletosTotales = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
+            this.tabBoletos.SuspendLayout();
+            this.tabRegistro.SuspendLayout();
+            this.tabConsultas.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblDistancia
             // 
             this.lblDistancia.AutoSize = true;
             this.lblDistancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDistancia.Location = new System.Drawing.Point(23, 131);
+            this.lblDistancia.Location = new System.Drawing.Point(6, 9);
             this.lblDistancia.Name = "lblDistancia";
             this.lblDistancia.Size = new System.Drawing.Size(132, 24);
             this.lblDistancia.TabIndex = 0;
@@ -50,16 +57,18 @@
             // 
             // txtDistancia
             // 
-            this.txtDistancia.Location = new System.Drawing.Point(258, 131);
+            this.txtDistancia.Location = new System.Drawing.Point(241, 9);
             this.txtDistancia.Name = "txtDistancia";
             this.txtDistancia.Size = new System.Drawing.Size(120, 20);
             this.txtDistancia.TabIndex = 1;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            this.txtDistancia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDistancia_KeyPress);
             // 
             // Distancia
             // 
             this.Distancia.AutoSize = true;
             this.Distancia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Distancia.Location = new System.Drawing.Point(23, 176);
+            this.Distancia.Location = new System.Drawing.Point(6, 54);
             this.Distancia.Name = "Distancia";
             this.Distancia.Size = new System.Drawing.Size(207, 24);
             this.Distancia.TabIndex = 2;
@@ -68,7 +77,7 @@
             // 
             // nudDias
             // 
-            this.nudDias.Location = new System.Drawing.Point(258, 181);
+            this.nudDias.Location = new System.Drawing.Point(241, 59);
             this.nudDias.Maximum = new decimal(new int[] {
             30,
             0,
@@ -93,7 +102,7 @@
             // 
             this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(80, 242);
+            this.btnCalcular.Location = new System.Drawing.Point(63, 104);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(75, 23);
             this.btnCalcular.TabIndex = 4;
@@ -105,7 +114,7 @@
             // 
             this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpiar.Location = new System.Drawing.Point(258, 242);
+            this.btnLimpiar.Location = new System.Drawing.Point(241, 104);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
             this.btnLimpiar.TabIndex = 5;
@@ -113,23 +122,69 @@
             this.btnLimpiar.UseVisualStyleBackColor = false;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
+            // tabBoletos
+            // 
+            this.tabBoletos.Controls.Add(this.tabRegistro);
+            this.tabBoletos.Controls.Add(this.tabConsultas);
+            this.tabBoletos.Location = new System.Drawing.Point(12, 12);
+            this.tabBoletos.Name = "tabBoletos";
+            this.tabBoletos.SelectedIndex = 0;
+            this.tabBoletos.Size = new System.Drawing.Size(432, 170);
+            this.tabBoletos.TabIndex = 6;
+            // 
+            // tabRegistro
+            // 
+            this.tabRegistro.Controls.Add(this.Distancia);
+            this.tabRegistro.Controls.Add(this.btnLimpiar);
+            this.tabRegistro.Controls.Add(this.lblDistancia);
+            this.tabRegistro.Controls.Add(this.btnCalcular);
+            this.tabRegistro.Controls.Add(this.txtDistancia);
+            this.tabRegistro.Controls.Add(this.nudDias);
+            this.tabRegistro.Location = new System.Drawing.Point(4, 22);
+            this.tabRegistro.Name = "tabRegistro";
+            this.tabRegistro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRegistro.Size = new System.Drawing.Size(424, 136);
+            this.tabRegistro.TabIndex = 0;
+            this.tabRegistro.Text = "Càlculo boletos";
+            this.tabRegistro.UseVisualStyleBackColor = true;
+            // 
+            // tabConsultas
+            // 
+            this.tabConsultas.Controls.Add(this.lstBoletosTotales);
+            this.tabConsultas.Location = new System.Drawing.Point(4, 22);
+            this.tabConsultas.Name = "tabConsultas";
+            this.tabConsultas.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConsultas.Size = new System.Drawing.Size(424, 144);
+            this.tabConsultas.TabIndex = 1;
+            this.tabConsultas.Text = "Consultas";
+            this.tabConsultas.UseVisualStyleBackColor = true;
+            // 
+            // lstBoletosTotales
+            // 
+            this.lstBoletosTotales.FormattingEnabled = true;
+            this.lstBoletosTotales.Location = new System.Drawing.Point(7, 4);
+            this.lstBoletosTotales.Name = "lstBoletosTotales";
+            this.lstBoletosTotales.Size = new System.Drawing.Size(411, 134);
+            this.lstBoletosTotales.TabIndex = 0;
+            this.lstBoletosTotales.SelectedIndexChanged += new System.EventHandler(this.lstBoletosTotales_SelectedIndexChanged);
+            // 
             // frmBoletos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 379);
-            this.Controls.Add(this.btnLimpiar);
-            this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.nudDias);
-            this.Controls.Add(this.Distancia);
-            this.Controls.Add(this.txtDistancia);
-            this.Controls.Add(this.lblDistancia);
+            this.ClientSize = new System.Drawing.Size(453, 223);
+            this.Controls.Add(this.tabBoletos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmBoletos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cálculo de boletos de tren";
             this.Load += new System.EventHandler(this.frmBoletos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
+            this.tabBoletos.ResumeLayout(false);
+            this.tabRegistro.ResumeLayout(false);
+            this.tabRegistro.PerformLayout();
+            this.tabConsultas.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -141,5 +196,9 @@
         private System.Windows.Forms.NumericUpDown nudDias;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TabControl tabBoletos;
+        private System.Windows.Forms.TabPage tabRegistro;
+        private System.Windows.Forms.TabPage tabConsultas;
+        private System.Windows.Forms.ListBox lstBoletosTotales;
     }
 }
--- SP02/frmBoletos.cs
+++ SP02/frmBoletos.cs
@@ -29,6 +29,12 @@ namespace SP02
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
+            if (txtDistancia.Text == "")
+            {
+                MessageBox.Show("Debe ingresar una distancia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             int distancia = int.Parse(txtDistancia.Text);
             int dias = (int)nudDias.Value;
             const int PRECIO_POR_KM = 5;
@@ -36,7 +42,17 @@ namespace SP02
             int precioBase = distanciaTotal * PRECIO_POR_KM;
             bool hayDescuento = (dias >= 7) && (distancia >= 100);
             int precioFinal = hayDescuento ? (precioBase / 2) : precioBase;
-           
+
+            string textoDescuento = "No";
+            if (hayDescuento)
+            {
+                textoDescuento = "Sí";
+            }
+
+            string textoParaLista = "Distancia: " + distancia + " km, Días: " + dias + ", Descuento: " + textoDescuento + " - Total: $" + precioFinal;
+
+            lstBoletosTotales.Items.Add(textoParaLista);
+
             MessageBox.Show(
                 "Distancia de ida: " + distancia + " km\n" +
                 "Distancia total: " + distanciaTotal + " km\n" +
@@ -46,7 +62,7 @@ namespace SP02
                 "Cálculo de boleto",
                 MessageBoxButtons.OK,
                 MessageBoxIcon.Information
-                            );
+                );
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
@@ -61,6 +77,28 @@ namespace SP02
         }
 
         private void Distancia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDistancia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void lstBoletosTotales_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
